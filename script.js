8b877102e6ea8958bd2a74d4640208bfaa996647
rclee91 <32626950+rclee91@users.noreply.github.com>
2021-01-14 00:11:00 -0500

    Minor layout changes

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
-function timeTo(time){
+timeToTT = () =>{
     let d = new Date();
-    let d2 = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), time, 0 , 0));
+    let d2 = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), 4, 0 , 0));
 
     let total = d2.getTime() - d.getTime();
-    console.log(total);
     if(total <= 0) total += 864000000;
 
     let seconds = Math.floor((total/1000)%60);
     let minutes = Math.floor((total/1000/60)%60);
     let hours = Math.floor((total/1000/60/60)%24);
 
-    console.log(total);
-
-    return {
-        total,
-        hours,
-        minutes,
-        seconds
-    }; 
+    return {total,hours,minutes,seconds}; 
 }
 
 displayClock = function(){
     if(document.querySelector("#tt-hours")){
         const timeinterval = setInterval(()=>{
-            let tt = timeTo(4); //utc 4:00
-            let setup = timeTo(3); //utc 3:00
+            let tt = timeToTT(); //utc 4:00
 
             if(tt.hours >= 23 && tt.minutes >= 30){
                 document.querySelector("#tt-timer").innerHTML= `TT may be Ongoing!! Check LFG or yell in guild chat`;
@@ -35,21 +26,17 @@ displayClock = function(){
                 document.querySelector("#tt-minutes").innerHTML = `${tt.minutes}`;    
                 document.querySelector("#tt-seconds").innerHTML = `${tt.seconds}`;
 
-                if(setup.hours < 1){
+                if(tt.hours <1 && tt.minutes > 30){
                     document.querySelector("#setup-timer").innerHTML = `Setup Ongoing!!`;
                 }
-                else{
-                    document.querySelector("#st-hours").innerHTML = `${setup.hours}`;
-                    document.querySelector("#st-minutes").innerHTML = `${setup.minutes}`;
-                    document.querySelector("#st-seconds").innerHTML = `${setup.seconds}`;
+                else if(tt.hours <1 && tt.minutes <= 30){
+                    document.querySelector("#setup-timer").innerHTML = `Taxi up! Check LFG or yell in guild chat`;
                 }
-            }
-
-    
-            
-            
-            if(tt.total< 0){
-                clearInterval(timeinterval);
+                else{
+                    document.querySelector("#st-hours").innerHTML = `${tt.hours -1}`;
+                    document.querySelector("#st-minutes").innerHTML = `${tt.minutes}`;
+                    document.querySelector("#st-seconds").innerHTML = `${tt.seconds}`;
+                }
             }
         },1000);
     } 
