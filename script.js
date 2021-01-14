f6948da18695ea295d0f3f5eb33da273215ea788
rclee91 <32626950+rclee91@users.noreply.github.com>
2021-01-14 03:45:15 -0500

    Added copy to clipboard
    And some style updates
    and disclaimer

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+quickCopy = (id) =>{
+    let current = document.getElementById(id);
+    let input = current.previousElementSibling;
+    
+    input.select();
+    input.setSelectionRange(0, 99999);
+    
+    document.execCommand("copy");
+};
+
 timeToTT = () =>{
     let d = new Date();
     let d2 = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), 4, 0 , 0));
@@ -10,47 +20,46 @@ timeToTT = () =>{
     let hours = Math.floor((total/1000/60/60)%24);
 
     return {total,hours,minutes,seconds}; 
-}
+};
 
-displayClock = function(){
-    if(document.querySelector("#tt-hours")){
+displayClock = () =>{
+    if(document.querySelector("#tt-clock")){
         const timeinterval = setInterval(()=>{
-            let tt = timeToTT(); //utc 4:00
+            let t = timeToTT(); //utc 4:00
+            let tt = document.querySelector("#tt-timer");
 
-            if(tt.hours >= 23 && tt.minutes >= 30){
-                document.querySelector("#tt-timer").innerHTML= `TT may be Ongoing!! Check LFG or yell in guild chat`;
-                document.querySelector("#setup-timer").innerHTML = `TT may be Ongoing!! Check LFG or yell in guild chat`;
+            if(t.hours >= 23 && tt.minutes >= 30){
+                tt.innerHTML= `TT may be Ongoing!! Check LFG or yell in guild chat`;
+                tt.style.color = "#7a0404";
+                tt.style.fontSize = "larger";
             }
             else{
-                document.querySelector("#tt-hours").innerHTML = `${tt.hours}`; 
-                document.querySelector("#tt-minutes").innerHTML = `${tt.minutes}`;    
-                document.querySelector("#tt-seconds").innerHTML = `${tt.seconds}`;
+                document.querySelector("#tt-hours").innerHTML = `${t.hours}`; 
+                document.querySelector("#tt-minutes").innerHTML = `${t.minutes}`;    
+                document.querySelector("#tt-seconds").innerHTML = `${t.seconds}`;
 
-                if(tt.hours <1 && tt.minutes > 30){
-                    document.querySelector("#setup-timer").innerHTML = `Setup Ongoing!!`;
+                if(t.hours <1 && t.minutes > 30){
+                    document.querySelector("#announce").innerHTML = `Setup Ongoing!!`;
                 }
-                else if(tt.hours <1 && tt.minutes <= 30){
-                    document.querySelector("#setup-timer").innerHTML = `Taxi up! Check LFG or yell in guild chat`;
+                else if(t.hours <1 && t.minutes <= 30){
+                    document.querySelector("#announce").innerHTML = `Taxi up! Check LFG or yell in guild chat`;
                 }
-                else{
-                    document.querySelector("#st-hours").innerHTML = `${tt.hours -1}`;
-                    document.querySelector("#st-minutes").innerHTML = `${tt.minutes}`;
-                    document.querySelector("#st-seconds").innerHTML = `${tt.seconds}`;
-                }
+
             }
         },1000);
     } 
-}
+};
 
-function modifyDate(){
+modifyDate = () =>{
     if(document.querySelector(".modify-date")){
         let d = new Date(document.lastModified);
         let options = {month: 'long'}
         document.querySelector(".modify-date").innerHTML = `Updated: ${new Intl.DateTimeFormat('en-US', options).format(d)}/${d.getDate()}/${d.getFullYear()}`;
+        document.querySelector(".modify-date").style.fontSize="x-small";
     }
-}
+};
 
-window.onload = function(){
+window.onload = () => {
     displayClock();
     modifyDate();
-}
+};
